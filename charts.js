1b3a6468d5337c336a148bf48c83d9ebf1b7dd3f
Sangster <bs281107@gmail.com>
2023-02-22 22:30:13 -0500

    Updated up to bar chart

--- charts.js
+++ charts.js
@@ -61,37 +61,58 @@ function buildCharts(sample) {
     console.log(data);
 
     // Deliverable 1: 3. Create a variable that holds the samples array. 
-
+    var samplesArray = data.sample;
+   
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samplesArray.filter(sampleObj => sampleObj.id == sample);
+    
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadataArray = data.metadata;
+    var resultsMetadata = metadataArray.filter(sampleObj => sampleObj.id == sample);
 
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
+    var firstSample = resultArray[0];
+    console.log(firstSample)
 
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
+    var firstMetadata = resultsMetadata[0];
+    console.log(firstMetadata);
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otuIds = firstSample.otu_ids;
+    var otuLabels = firstSample.otu_labels;
+    var sampleValues = firstSample.sample_values;
+     
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
-
+    var washFreq = parseFloat(firstMetadata.washFreq);
 
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
     // so the otu_ids with the most bacteria are last. 
-    var yticks = 
+    var yticks = otuIds.slice(0, 10).map(id => "OTU " + id + " ").reverse();
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
-    var barData = [
+    var barData = [{
+      x: sampleValues.slice(0, 10).reverse(),
+      y: yticks,
+      text: otuLabels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation:"h",
+      marker: {
+        color: sampleValues.slice(o, 10).reverse(),
+        colorscale: "geyser"
 
-    ];
+      }  
+    }];
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
     var barLayout = {
-
-    };
+      title: "TOP 10 Bacteria Cultures Found"
+    }
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
-
+    Plotly.newPlot("bar", barData, barLayout);
+    
     // Deliverable 2: 1. Create the trace for the bubble chart.
 
     // Deliverable 2: 2. Create the layout for the bubble chart.
